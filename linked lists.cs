5f8d7f44059242be1a276894fc0fa03b02fba0e2
andre <andre.alvarez@ucsp.edu.pe>
2020-01-27 13:03:41 -0500

    Add files via upload

---
+++ linked lists.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> lista = new List<int>();
+            lista.Add(1);
+            lista.Add(4);
+            lista.Add(6);
+            lista.Add(10);
+
+            int p = 0;
+
+            Console.WriteLine();
+            foreach (int k in lista)
+            {
+                Console.Write("At Position {0}: ", p);
+                Console.WriteLine(k);
+                p++;
+            }
+
+            Console.WriteLine();
+            p = 0;
+            lista.Remove(4);
+            foreach (int k in lista)
+            {
+                Console.Write("At Position {0}: ", p);
+                Console.WriteLine(k);
+                p++;
+            }
+
+            Console.WriteLine();
+            lista.Sort();
+            Console.WriteLine("Sorted List:");
+            p = 0;
+            foreach (int k in lista)
+            {
+                Console.Write("At Position {0}: ", p);
+                Console.WriteLine(k);
+            }
+
+        }
+    }
+}
